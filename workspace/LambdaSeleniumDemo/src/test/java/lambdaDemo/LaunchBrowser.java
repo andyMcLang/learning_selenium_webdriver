73030c52e1e5c972d6a6719031c5ac8021e158f5
Andy Lang <djdreviz@gmail.com>
2025-03-17 02:04:42 +0200

    first Selenium test done!

---
+++ workspace/LambdaSeleniumDemo/src/test/java/lambdaDemo/LaunchBrowser.java
@@ -0,0 +1,19 @@
+package lambdaDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchBrowser {
+
+	@Test
+	public void myTest() {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.lambdatest.com/");
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.quit();
+	}
+
+}
